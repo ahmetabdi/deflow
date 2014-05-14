772c0ea1f99b3079c410c3ddfd6b48694492298e
Ahmet <ahmet@kyan.com>
2014-05-14 09:22:30 +0100

    More logic

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -18,7 +18,9 @@ class AppDelegate
 
     # NSWorkspace.sharedWorkspace.setDesktopImageURL(path, forScreen: NSScreen.screens.lastObject, options: nil, error: nil)
 
-    saveImagesInLocalDirectory("https://www.google.co.uk/logos/doodles/2014/dorothy-hodgkins-104th-birthday-born-1910-5134139112030208.3-hp.jpg")
+    createStorageFolder
+    saveImage("https://www.google.co.uk/logos/doodles/2014/dorothy-hodgkins-104th-birthday-born-1910-5134139112030208.3-hp.jpg")
+    deleteAllFiles
   end
 
   def createMenuItem(name, action)
@@ -34,27 +36,56 @@ class AppDelegate
     object.setState NSOnState
   end
 
-  def saveImagesInLocalDirectory(imgURL)
+  def createStorageFolder
+    docDir = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true).first
+
+    directory = docDir.stringByAppendingPathComponent("Deflow")
+
+    fileManager = NSFileManager.defaultManager
+    if(!fileManager.createDirectoryAtPath(directory, withIntermediateDirectories:true, attributes: nil, error: nil))
+      NSLog("Error: Create folder failed: %@", directory)
+    else
+      NSLog("Successfully created folder: %@", directory)
+    end
+  end
+
+  def saveImage(imgURL)
     documentsDirectoryPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true).first
-    imgName = "image.png"
+    imgName = "Deflow/image.png"
     fileManager = NSFileManager.defaultManager
     writablePath = documentsDirectoryPath.stringByAppendingPathComponent(imgName)
 
     if(!fileManager.fileExistsAtPath(writablePath))
-        NSLog("file doesn't exist");
-        # Save Image From URL
+        NSLog("%@ doesn't exist creating...", imgName);
         data = NSData.dataWithContentsOfURL(NSURL.URLWithString(imgURL))
 
         error = nil
-        data.writeToFile(documentsDirectoryPath.stringByAppendingPathComponent(NSString.stringWithFormat("%@", imgName)), options:NSAtomicWrite, error: error)
+        data.writeToFile(documentsDirectoryPath.stringByAppendingPathComponent(
+                                    NSString.stringWithFormat("%@", imgName)),
+                                    options:NSAtomicWrite, error: error)
 
       if (error)
-        NSLog("Error Writing File : %@",error);
+        NSLog("Error Writing File: %@", error);
       else
-        NSLog("Image %@ Saved SuccessFully",imgName);
+        NSLog("%@ Saved SuccessFully", imgName);
       end
     else
-      NSLog("file exist");
+      NSLog("%@ already exists skipping...", imgName)
+    end
+  end
+
+  def deleteAllFiles
+    fm = NSFileManager.defaultManager
+
+    directory = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true).first
+    directory = directory.stringByAppendingPathComponent("Deflow")
+    fm.contentsOfDirectoryAtPath(directory, error: nil).each do |file|
+        success = fm.removeItemAtPath(NSString.stringWithFormat("%@/%@", directory, file), error: nil)
+        if !success
+          NSLog("Failed to delete file: #{file}")
+        else
+          NSLog("Successfully deleted file: #{file}")
+        end
     end
   end
 
